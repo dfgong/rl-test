1839d492b3e175b8a99b3e80654bde4ff45866f5
gongdingfei <156821840@qq.com>
2019-12-06 18:21:38 +0800

    tijoa

---
+++ my-app/src/App.js
@@ -0,0 +1,137 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: '', value1: ['']};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleChange1 = this.handleChange1.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleChange1(event) {
+    this.setState({value1: this.state.value1.push(event.target.value)});
+  }
+
+  handleSubmit(event) {
+    alert('提交的名字: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <select multiple={true} value={['grapefruit', 'lime']} onChange={this.handleChange1}>
+            <option value="grapefruit">葡萄柚</option>
+            <option value="lime">酸橙</option>
+            <option value="coconut">椰子</option>
+            <option value="mango">芒果</option>
+          </select>
+        <label>
+          名字:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="提交" />
+      </form>
+    );
+  }
+}
+function Welcome(props) {
+  return <h1>Hello Welcome,{props.name}</h1>
+}
+class Welcome2 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    this.props.onChangeShareName('gongdf2')
+  }
+  render() {
+    return <h1>
+      Hello Welcome2, <span onClick={this.handleClick}>{this.props.name}</span>
+      </h1>;
+  }
+}
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: new Date(),
+      name: 'gongdf1',
+      shareName: 'gongdf',
+    }
+    this.changeShareName = this.changeShareName.bind(this);
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState((state, props) => {
+      return {
+        date: new Date(),
+        name: 'gongdf' + (state.name.substring(6)-0+1)
+      };
+    });
+  }
+
+  changeShareName(val) {
+    this.setState({
+      shareName: val,
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+        <Welcome2 onChangeShareName={this.changeShareName} name={this.state.shareName}></Welcome2>
+        <Welcome name={this.state.shareName}></Welcome>
+      </div>
+    );
+  }
+}
+
+function App() {
+  return (
+    <div className="App">
+      <NameForm></NameForm>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <Clock></Clock>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ test.js
@@ -0,0 +1,24 @@
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    // return e(
+    //   'button',
+    //   { onClick: () => this.setState({ liked: true }) },
+    //   'Like'
+    // );
+    return <button>Like</button>
+  }
+}
+
+const domContainer = document.querySelector('#root');
+ReactDOM.render(e(LikeButton), domContainer);
